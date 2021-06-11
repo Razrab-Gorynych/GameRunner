8c338214ce38a72ce5165c3750c04c3fe6e4d788
Razrab-Gorynych <cxzxz69@gmail.com>
2021-06-11 17:19:06 +0300

    Jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Windows;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    public Vector3 dir;
+    [SerializeField] public float JumpForce;
+    [SerializeField] public float gravity;
+    
+
+    void Start() 
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { if (controller.isGrounded)
+                Jump();
+        } 
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(dir * Time.fixedDeltaTime);
+        dir.y += gravity * Time.fixedDeltaTime;
+    }
+
+    void Jump()
+    {
+        dir.y = JumpForce;
+    }
+}
